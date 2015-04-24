aa6861b13e7180101f700c6b236e76a98c70d127
Luis Hernandez <lhernandez@colum.edu>
2015-04-24 14:49:47 -0500

    populated seller and location

--- db/update_tables/update_listings_table.rb
+++ db/update_tables/update_listings_table.rb
@@ -1,6 +1,11 @@
 require "../../config/environment"
 
 Listing.all.each do |list|
-	Seller.create(name: list.seller_name)
-	Location.create(name: list.location_name, address: list.address, city: list.city)
+	new_location = Location.create(name: list.location_name, address: list.address, city: list.city)
+	list.update_attributes(location_id: new_location.id)
+	new_seller = Seller.create(name: list.seller_name)
+	new_location.update_attributes(seller_id: new_seller.id)
 end 
+
+
+# are locations unique? same name... different address. can be refactored... validations
